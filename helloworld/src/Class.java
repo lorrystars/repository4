821cafbe3e5c60c7abcb7e54a04652f29a5dfc1a
lorrystars <54662044+lorrystars@users.noreply.github.com>
2019-08-29 21:40:41 +0800

    Update Class.java

--- helloworld/src/Class.java
+++ helloworld/src/Class.java
@@ -5,6 +5,7 @@ public class Class {
         System.out.println("git在idea中的应用");
         System.out.println("第一次克隆修改操作");
         System.out.println("克隆后第二次重新修改");
+        System.out.println("远程仓库中修改文件传送到本地文件夹");
     }
 
 }
