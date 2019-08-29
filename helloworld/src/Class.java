a5a46028108ada6e03ddffb4425f538b9b5cded0
lorry <1456105309@qq.com>
2019-08-29 21:17:55 +0800

    idea第三次提交

--- helloworld/src/Class.java
+++ helloworld/src/Class.java
@@ -2,6 +2,7 @@ public class Class {
     public static void main(String[] args) {
         System.out.println("helloworld");
         System.out.println("idea第二次提交");
+        System.out.println("git在idea中的应用");
     }
 
 }
