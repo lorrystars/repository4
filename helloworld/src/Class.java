52c43b9f2923ed9cd9059aa316ac46c0ef970fa0
lorry <1456105309@qq.com>
2019-08-29 21:32:42 +0800

    clone版本提交到GitHub远程仓库中

--- helloworld/src/Class.java
+++ helloworld/src/Class.java
@@ -3,6 +3,7 @@ public class Class {
         System.out.println("helloworld");
         System.out.println("idea第二次提交");
         System.out.println("git在idea中的应用");
+        System.out.println("第一次克隆修改操作");
     }
 
 }
