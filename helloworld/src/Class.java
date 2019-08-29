52284e0e549a52a99a913ec7943bd4e955741943
lorry <1456105309@qq.com>
2019-08-29 21:37:36 +0800

    clone后第二次修改

--- helloworld/src/Class.java
+++ helloworld/src/Class.java
@@ -4,6 +4,7 @@ public class Class {
         System.out.println("idea第二次提交");
         System.out.println("git在idea中的应用");
         System.out.println("第一次克隆修改操作");
+        System.out.println("克隆后第二次重新修改");
     }
 
 }
